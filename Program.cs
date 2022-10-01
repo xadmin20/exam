c9941faf9c04f078dfe02fd6edeb4e60f3dc0f1b
Nick <74838258+xadmin20@users.noreply.github.com>
2022-10-01 11:34:52 +0300

    Update Program.cs
    Добавил запрос на ввод данных в массив, разбил массив на переменные

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите данные через пробел");
+string stroka = Console.ReadLine();
+string[] spl;
+spl = stroka.Split(' ');
+var result = new string[spl.Length];
+var rSize = 0;
